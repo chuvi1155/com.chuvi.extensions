0bad313eae877bae48f553f7a379eacd1346eb87
chuvi1155 <chuvi1155@users.noreply.github.com>
2021-06-27 20:10:11 +0300

    Update StringExtentions.cs

--- Runtime/StringExtentions.cs
+++ Runtime/StringExtentions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 public static class StringExtentions
 {
     public static string After(this string s, string value)
@@ -150,5 +151,23 @@ public static class StringExtentions
         double res;
         double.TryParse(s, out res);
         return res;
+    }
+    /// <summary>
+    /// перемешать список
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="list"></param>
+    public static void Shuffle<T>(this IList<T> list)
+    {
+        Random random = new Random();
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = random.Next(n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
     }
 }
