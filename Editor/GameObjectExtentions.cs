71242bf12c1d981b2034fd4521dadc698f1a59ef
chuvi1155 <chuvi1155@users.noreply.github.com>
2022-03-30 16:55:37 +0300

    Add files via upload

---
+++ Editor/GameObjectExtentions.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Collections.Generic;
+
+[InitializeOnLoad]
+public class GameObjectExtentions : MonoBehaviour
+{
+    static Texture2D texture;
+    static Dictionary<int, GameObject> objects;
+    //static GUIStyle btnStyle;
+    //static GUIStyle boxStyle;
+
+    static GameObjectExtentions()
+    {
+        texture = Texture2D.whiteTexture;
+
+        
+
+        //btnStyle = new GUIStyle();
+        //btnStyle.normal.background = texture;
+        //btnStyle.hover.background = texture;
+        //btnStyle.active.background = texture;
+
+        EditorApplication.update += UpdateCB;
+        EditorApplication.hierarchyWindowItemOnGUI += HierarchyItemCB;
+    }
+
+    static void UpdateCB()
+    {
+        // Check here
+        //GameObject[] go1 = Object.FindObjectsOfType(typeof(GameObject)) as GameObject[];
+        GameObject[] go1 = Resources.FindObjectsOfTypeAll<GameObject>();
+        if (objects == null) objects = new Dictionary<int, GameObject>();
+        else objects.Clear();
+
+        foreach (GameObject g in go1)
+        {
+            objects.Add(g.GetInstanceID(), g);
+        }
+    }
+
+    static void HierarchyItemCB(int instanceID, Rect selectionRect)
+    {
+        if (objects == null ) return;
+        if (objects.ContainsKey(instanceID) && objects[instanceID] != null)
+        {
+            Rect r = new Rect(selectionRect);
+            r.x = r.xMax - 22;
+            r.width = 18;
+            r.height = 18;
+            //r.y += 4;
+            Color col = GUI.color;
+            //GUI.color = markedSublines[instanceID].curveColor;
+            //GUI.DrawTexture(r, texture);
+            objects[instanceID].SetActive(GUI.Toggle(r, objects[instanceID].activeSelf, ""));
+            GUI.color = col;
+        }
+    }
+}
