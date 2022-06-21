de4ec889fa0804d8982034a94795b3d899dd33e5
Илья Ткаченко <iliaakm@msk.gefestcapital.ru>
2022-06-21 16:56:02 +0300

    Update GameObjectExtentions.cs

--- Editor/GameObjectExtentions.cs
+++ Editor/GameObjectExtentions.cs
@@ -8,19 +8,10 @@ public class GameObjectExtentions : MonoBehaviour
 {
     static Texture2D texture;
     static Dictionary<int, GameObject> objects;
-    //static GUIStyle btnStyle;
-    //static GUIStyle boxStyle;
 
     static GameObjectExtentions()
     {
         texture = Texture2D.whiteTexture;
-
-        
-
-        //btnStyle = new GUIStyle();
-        //btnStyle.normal.background = texture;
-        //btnStyle.hover.background = texture;
-        //btnStyle.active.background = texture;
 
         EditorApplication.update += UpdateCB;
         EditorApplication.hierarchyWindowItemOnGUI += HierarchyItemCB;
@@ -28,8 +19,6 @@ public class GameObjectExtentions : MonoBehaviour
 
     static void UpdateCB()
     {
-        // Check here
-        //GameObject[] go1 = Object.FindObjectsOfType(typeof(GameObject)) as GameObject[];
         GameObject[] go1 = Resources.FindObjectsOfTypeAll<GameObject>();
         if (objects == null) objects = new Dictionary<int, GameObject>();
         else objects.Clear();
@@ -49,17 +38,15 @@ public class GameObjectExtentions : MonoBehaviour
             r.x = r.xMax - 22;
             r.width = 18;
             r.height = 18;
-            //r.y += 4;
-            //Color col = GUI.color;
-            //GUI.color = markedSublines[instanceID].curveColor;
-            //GUI.DrawTexture(r, texture);
             bool res = GUI.Toggle(r, objects[instanceID].activeSelf, "");
             if (GUI.changed)
             {
-                objects[instanceID].SetActive(res);
-                EditorUtility.SetDirty(objects[instanceID]);
+                if (res != objects[instanceID].activeSelf)
+                {
+                    objects[instanceID].SetActive(res);
+                    EditorUtility.SetDirty(objects[instanceID]); 
+                }
             }
-            //GUI.color = col;
         }
     }
 }
