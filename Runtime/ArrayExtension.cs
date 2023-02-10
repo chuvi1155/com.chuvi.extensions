00c105f1895885274afe6e9cea6ca6cdfb66492f
Stanislav Blinov <chuvi1155@users.noreply.github.com>
2023-02-10 14:35:40 +0300

    Add files via upload

---
+++ Runtime/ArrayExtension.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+public static class ArrayExtension 
+{
+    internal static byte[] ToByteArray(this float[] data)
+    {
+        byte[] a = new byte[data.Length * sizeof(float)];
+        IntPtr ptr = Marshal.AllocHGlobal(a.Length);
+        Marshal.Copy(data, 0, ptr, data.Length);
+        Marshal.Copy(ptr, a, 0, a.Length);
+        Marshal.FreeHGlobal(ptr);
+        return a;
+    }
+
+    internal static byte[] ToByteArray(this int[] data)
+    {
+        byte[] a = new byte[data.Length * sizeof(int)];
+        IntPtr ptr = Marshal.AllocHGlobal(a.Length);
+        Marshal.Copy(data, 0, ptr, data.Length);
+        Marshal.Copy(ptr, a, 0, a.Length);
+        Marshal.FreeHGlobal(ptr);
+        return a;
+    }
+    //internal static T[] FromDataArray<T>(this byte[] data) where T : struct
+    //{
+    //    T[] a = new T[data.Length / Marshal.SizeOf<T>()];
+    //    IntPtr ptr = Marshal.AllocHGlobal(a.Length);
+    //    Marshal.Copy(data, 0, ptr, data.Length);
+    //    if (typeof(T) == typeof(int))
+    //    {
+    //        int[] arr = (int[])(object)a;
+    //        Marshal.Copy(ptr, arr, 0, a.Length);
+    //    }
+    //    else if (typeof(T) == typeof(float))
+    //    {
+    //        float[] arr = (float[])(object)a;
+    //        Marshal.Copy(ptr, arr, 0, a.Length);
+    //    }
+    //    else if (typeof(T) == typeof(UnityEngine.Color))
+    //    {
+    //        UnityEngine.Color[] arr = (UnityEngine.Color[])(object)a;
+    //        Marshal.Copy(ptr, arr, 0, a.Length);
+    //    }
+    //    Marshal.FreeHGlobal(ptr);
+    //    return a;
+    //}
+    public static T[] FromByteArray<T>(this byte[] source) where T : struct
+    {
+        T[] destination = new T[source.Length / Marshal.SizeOf(typeof(T))];
+        GCHandle handle = GCHandle.Alloc(destination, GCHandleType.Pinned);
+        try
+        {
+            IntPtr pointer = handle.AddrOfPinnedObject();
+            Marshal.Copy(source, 0, pointer, source.Length);
+            return destination;
+        }
+        finally
+        {
+            if (handle.IsAllocated)
+                handle.Free();
+        }
+    }
+    public static byte[] ToByteArray<T>(T[] source) where T : struct
+    {
+        GCHandle handle = GCHandle.Alloc(source, GCHandleType.Pinned);
+        try
+        {
+            IntPtr pointer = handle.AddrOfPinnedObject();
+            byte[] destination = new byte[source.Length * Marshal.SizeOf(typeof(T))];
+            Marshal.Copy(pointer, destination, 0, destination.Length);
+            return destination;
+        }
+        finally
+        {
+            if (handle.IsAllocated)
+                handle.Free();
+        }
+    }
+}
---
+++ Runtime/XCodeLogHelper.cs
@@ -0,0 +1,52 @@
+using System;
+using UnityEngine;
+
+public class XCodeLogHelper : ILogHandler
+{
+    public bool enable;
+    private static ILogHandler unityLogHandler = Debug.unityLogger.logHandler;
+
+    public XCodeLogHelper(bool enable = true)
+    {
+        this.enable = enable;
+    }
+
+    public void LogException(Exception exception, UnityEngine.Object context)
+    {
+        if (enable)
+        {
+            exception = new Exception("📕📕 `; " + exception.Message, exception);
+            unityLogHandler.LogException(exception, context);
+        }
+    }
+
+    public void LogFormat(LogType logType, UnityEngine.Object context, string format, params object[] args)
+    {
+//#if UNITY_EDITOR || DEVELOPMENT_BUILD
+        if (enable)
+        {
+            switch (logType)
+            {
+                case LogType.Log:
+                    format = "📗 : " + format;
+                    break;
+                case LogType.Warning:
+                    format = "📙 : " + format;
+                    break;
+                case LogType.Error:
+                    format = "📕 : " + format;
+                    break;
+                case LogType.Exception:
+                    format = "📕📕 : " + format;
+                    break;
+                case LogType.Assert:
+                    format = "📘 : " + format;
+                    break;
+                default:
+                    break;
+            }
+            unityLogHandler.LogFormat(logType, context, format, args);
+        }
+//#endif
+    }
+}
