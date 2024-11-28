e5731f297762e12374f9f353cfeb623c641332db
Stanislav Blinov <chuvi1155@users.noreply.github.com>
2024-11-28 13:28:35 +0300

    Add files via upload

---
+++ Editor/ProjectCreater.cs
@@ -0,0 +1,47 @@
+using UnityEditor;
+using UnityEngine;
+
+public class ProjectCreater : MonoBehaviour
+{
+    [MenuItem("ProjectCreater/Create template project", false, 0)]
+    public static void CreateTemplateProject()
+    {
+        if (!AssetDatabase.IsValidFolder("Assets/Scripts"))
+            AssetDatabase.CreateFolder("Assets", "Scripts");
+        if (!AssetDatabase.IsValidFolder("Assets/UI"))
+            AssetDatabase.CreateFolder("Assets", "UI");
+        if (!AssetDatabase.IsValidFolder("Assets/UI/Previews"))
+            AssetDatabase.CreateFolder("Assets/UI", "Previews");
+        if (!AssetDatabase.IsValidFolder("Assets/Resources"))
+            AssetDatabase.CreateFolder("Assets", "Resources");
+        if (!AssetDatabase.IsValidFolder("Assets/Resources/Screens"))
+            AssetDatabase.CreateFolder("Assets/Resources", "Screens");
+        if (!AssetDatabase.IsValidFolder("Assets/Prefabs"))
+            AssetDatabase.CreateFolder("Assets", "Prefabs");
+
+        AddTag("MainCanvas");
+    }
+
+    public static void AddTag(string tag)
+    {
+        UnityEngine.Object[] asset = AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset");
+        if ((asset != null) && (asset.Length > 0))
+        {
+            SerializedObject so = new SerializedObject(asset[0]);
+            SerializedProperty tags = so.FindProperty("tags");
+
+            for (int i = 0; i < tags.arraySize; ++i)
+            {
+                if (tags.GetArrayElementAtIndex(i).stringValue == tag)
+                {
+                    return;     // Tag already present, nothing to do.
+                }
+            }
+
+            tags.InsertArrayElementAtIndex(0);
+            tags.GetArrayElementAtIndex(0).stringValue = tag;
+            so.ApplyModifiedProperties();
+            so.Update();
+        }
+    }
+}
